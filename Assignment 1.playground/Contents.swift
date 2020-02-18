03cea9f3953a09e3fe8ebd232cae003be32c5003
akjohn97 <60237689+akjohn97@users.noreply.github.com>
2020-02-18 09:50:46 -0500

    Add files via upload

--- Assignment 1.playground/Contents.swift
+++ Assignment 1.playground/Contents.swift
@@ -38,3 +38,63 @@ print(numA)
 let aSet: Set<Int> = [3,4,4,3]
 var aTenerary = aSet.contains(3) ? "true" : "false"
 
+
+
+let aString = "Dog"
+let aInt = 5
+let aBool = true
+//String aString = "Dog";
+//int aInt = 5;
+//boolean aBool = true;
+
+let aDict = ["zero":2, "one":3, "two":4]
+for (key,value) in myDict{
+    print("Key: \(key) and Value: \(value)")
+}
+for key in myDict {
+    print(key)
+}
+//Map<String, int> dict = new HashMap<String, int>();
+//dict.put("zero":2);
+//for (Enumeration i = dict.elements(); i.hasMoreElements();)
+//  System.out.println("Value in Dictionary" + i.nextElement());
+//for(Enumeration i = dict.keys(); i.hasMoreElements();)
+//  System.out.println("Keys in Dictionary" + k.nextElement());
+
+
+func add() -> Int{
+    let sum = 1+2
+    return sum
+}
+//public int add(){
+//int sum 1+2;
+//return sum
+
+
+class Car {
+    var make: String
+    var speed: Int
+    init(make: string, speed: Int){
+        self.make = make
+        slef.speed = speed
+    }
+}
+//class Car{
+//String make;
+//int speed;
+//}
+
+struct Van{
+    var make: Int
+}
+//class Van{
+//int make;
+//}
+
+
+
+    }
+}
+
+
+
